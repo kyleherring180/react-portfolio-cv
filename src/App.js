d578b0f3cdd70a029ae893471bda97416219cfdd
kyleherring180 <55635978+kyleherring180@users.noreply.github.com>
2024-08-04 20:06:37 +0200

    router changes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+      </Routes>
     </>
   );
 }
