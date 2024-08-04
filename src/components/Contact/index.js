89e2aa1db539dfb0f53d3c704e9e414b2cb7004f
kyleherring180 <55635978+kyleherring180@users.noreply.github.com>
2024-08-04 22:15:00 +0200

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,7 +25,6 @@ const Contact = () => {
           .then(
             () => {
               alert('Message successfully sent!')
-              window.location.reload(true)
             },
             () => {
               alert('Failed to send the message, please try again')
