95bcd577f5fb5fdc373825abe69985ed999d47f7
kyleherring180 <55635978+kyleherring180@users.noreply.github.com>
2024-11-16 19:56:57 +0100

    Blog page update

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,16 +1,10 @@
-import { useEffect, useState } from 'react'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import Blog1Img from '../../assets/images/integration_tests_blog.png';
+import './index.scss';
+import {Card, CardContent, CardHeader, CardMedia, Typography} from "@mui/material";
 
-const blogPosts = [
-    {
-        id: 1,
-        image: '',
-        title: 'Integration Testing',
-        description: 'This is the first blog post.',
-    }
-];
 const Blog = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
 
@@ -32,9 +26,29 @@ const Blog = () => {
                         />
                     </h1>
                     <div className="blog-grid">
-                        {blogPosts.map((post) => (
-                            <div></div>
-                        ))}
+                        <div>
+                            <Card sx={{backgroundColor: '#0a5775',maxWidth: 345 }}>
+                                <CardHeader
+                                    sx={{color: '#fff'}}
+                                    title=".Net Test Containers for Integration tests using Microsoft SqlServer"
+                                    subheader="November 16, 2024"
+                                    subheaderTypographyProps={{ style: { color: '#fff' } }}
+                                />
+                                <CardMedia
+                                    component="img"
+                                    height="194"
+                                    image={Blog1Img}
+                                    alt="Integration Tests"
+                                />
+                                <CardContent>
+                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                        Discover how to streamline your integration testing process in .NET applications using Testcontainers with Microsoft SQL Server. 
+                                        This post explores how to set up isolated, lightweight containers for database integration tests, 
+                                        ensuring consistency and reliability across different environments.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </div>
                     </div>
                 </div>
             </div>
